d111eed5f86606de6bbb78911e044ce30d532900
beareid <beareid.info@gmail.com>
2022-06-19 06:37:02 -0500

    Work on style and functionality for slider component

--- scripts.js
+++ scripts.js
@@ -7,25 +7,25 @@ slider.addEventListener('input', (e) => {
   switch(e.target.value) {
     case '0': 
       pageNum.textContent = "10K";
-      price.textContent = "$8";
+      price.textContent = "$8.00";
       break;
     case '1': 
       pageNum.textContent = "50K";
-      price.textContent = "$12";
+      price.textContent = "$12.00";
       break;
     case '2': 
       pageNum.textContent = "100K";
-      price.textContent = "$16";
+      price.textContent = "$16.00";
       break;
     case '3': 
       pageNum.textContent = "500K";
-      price.textContent = "$24";
+      price.textContent = "$24.00";
       break;
     case '4': 
       pageNum.textContent = "1M";
-      price.textContent = "$36";
+      price.textContent = "$36.00";
       break;
     default:
       return;
   }
-})
+});
